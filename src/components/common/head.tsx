4a1fb75b7ad53ee8ce0514f8bcbad9b2c5aa280c
insik-han <gksdudrms0@icloud.com>
2023-04-09 22:59:48 +0900

    initial commit

---
+++ src/components/common/head.tsx
@@ -0,0 +1,11 @@
+import NextHead from "next/head";
+
+export default function Head() {
+  return (
+    <>
+      <NextHead>
+        <title>Next TS</title>
+      </NextHead>
+    </>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import 'src/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import { Head } from "src/components/common";
+import { fontPretendard } from "src/fonts";
+
+import "src/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head />
+      <style jsx global>{`
+        :root {
+          --font-pretendard: ${fontPretendard.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
